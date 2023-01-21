cbd265248e856b2d62cd1ec1879de9935321c6cd
Miguel Suau <miguel.suau@gmail.com>
2023-01-21 20:56:57 +0100

    back to original reward

--- environments/tmaze/tmaze.py
+++ environments/tmaze/tmaze.py
@@ -198,7 +198,7 @@ class Tmaze(gym.Env):
         # return new_location
 
     def reward_done(self, action):
-        # reward = -0.01
+        reward = -0.01
         reward = 0.0
         done = False
         if self.location[1] == self.CORRIDOR_LENGTH - 1:
@@ -206,14 +206,14 @@ class Tmaze(gym.Env):
                 done = True
                 if self.signal == -1:
                     reward = 1.0
-                # else:
-                    # reward = -1.0
+                else:
+                    reward = -1.0
             if self.location[0] == self.CORRIDOR_WIDTH-1 and action == 1:
                 done = True
                 if self.signal == 1:
                     reward = 1.0
-                # else:
-                    # reward = -1.0
+                else:
+                    reward = -1.0
             # if self.slippery:
             #     print(self.location, action, self.signal, reward, done)
         if self.steps >= self.max_steps:
